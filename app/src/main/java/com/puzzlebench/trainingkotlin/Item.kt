1ae77fd013d50a804d4dfdb4426b4cf80a66b99f
ads.f117 <ads.f117@gmail.com>
2017-07-17 20:41:38 -0500

    Fix issue resize image afther scroll

--- app/src/main/java/com/puzzlebench/trainingkotlin/Item.kt
+++ app/src/main/java/com/puzzlebench/trainingkotlin/Item.kt
@@ -2,7 +2,7 @@ package com.puzzlebench.trainingkotlin
 
 fun getItems(): List<Item>{
     //Rangos (1..10) manjeo de numeros secuenciales
-    return (1..10).map { Item(it.toLong(),"Title $it", "http://lorempixel.com/400/400/cats/$it/") }
+    return (1..10).map { Item(it.toLong(),"City $it", "http://lorempixel.com/400/400/city/$it/") }
 }
 
 class Item(val id: Long, val title: String, val url: String)
