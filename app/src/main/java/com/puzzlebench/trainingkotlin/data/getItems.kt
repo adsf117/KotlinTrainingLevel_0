66e6735c8d6412d6f36546d6264804cd8d26d30b
andres.serrano <aserrano@rccl.com>
2017-11-01 20:34:02 -0500

    fix range items generator(was removed the duplicate id)

--- app/src/main/java/com/puzzlebench/trainingkotlin/data/getItems.kt
+++ app/src/main/java/com/puzzlebench/trainingkotlin/data/getItems.kt
@@ -5,7 +5,7 @@ import com.puzzlebench.trainingkotlin.Item
 fun getVideoItems(): List<Item> =//Rangos (1..5) manjeo de numeros secuenciales
         (1..5).map { Item(it.toLong(), "City $it", "http://lorempixel.com/400/400/city/$it/", Item.Type.VIDEO) }
 
-fun getPhotoItems(): List<Item> = (5..10).map { Item(it.toLong(), "City $it", "http://lorempixel.com/400/400/city/$it/", Item.Type.PHOTO) }
+fun getPhotoItems(): List<Item> = (6..10).map { Item(it.toLong(), "City $it", "http://lorempixel.com/400/400/city/$it/", Item.Type.PHOTO) }
 
 fun getItems(): List<Item> =
         (getPhotoItems() + getVideoItems()).sortedBy { it.id }//by defaul return a inmutable list if we need a mutable list we cant use .toMutableList()
