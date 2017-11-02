6ad17e1b6285df58ce4e29ed7d654f903c42a48f
andres.serrano <aserrano@rccl.com>
2017-11-01 20:49:47 -0500

    Example inFix Funtion Extension

--- app/src/main/java/com/puzzlebench/trainingkotlin/inFixTest.kt
+++ app/src/main/java/com/puzzlebench/trainingkotlin/inFixTest.kt
@@ -3,6 +3,9 @@ package com.puzzlebench.trainingkotlin
 fun test (items: List<Item>){
     var sum1 = 7.addition(9)
     var suminFix = 7 additionInFix 9
+
+    var pair = Pair(7,"Day")
+    var pairInFix = 7 to "Day"//I dont like this, 7 to "Day" to 4 to 8 to ....n
 }
 
 fun Int.addition(anyNumber: Int) =this + anyNumber
